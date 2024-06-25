4eea344832515b42949d94bc4b9a79965878a31a
anas <azizortukov818@gmail.com>
2024-06-25 18:43:44 +0500

    initial commit

---
+++ src/main/java/uz/audio_book/backend/AudioBookBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.audio_book.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/audio_book/backend/AudioBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.audio_book.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
