241be9f777c2dd5549de9dc96abf3733bed071ab
azizortukov <azizortukov818@gmail.com>
2024-07-17 14:08:12 +0500

    testing directory added

---
+++ src/test/java/uz/audio_book/backend/service/BookServiceImplTest.java
@@ -0,0 +1,52 @@
+package uz.audio_book.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceImplTest {
+
+    @Test
+    void getBooksProjection() {
+    }
+
+    @Test
+    void getHomeData() {
+    }
+
+    @Test
+    void getAdminProjection() {
+    }
+
+    @Test
+    void saveBook() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void sendBookPicture() {
+    }
+
+    @Test
+    void sendBookPDF() {
+    }
+
+    @Test
+    void sendBookAudio() {
+    }
+
+    @Test
+    void getByAuthorOrTitle() {
+    }
+
+    @Test
+    void getSelected() {
+    }
+
+    @Test
+    void saveComment() {
+    }
+}
